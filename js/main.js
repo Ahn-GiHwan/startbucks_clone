2a36fcf64b2756e71f0edc7c473ad247a8526e75
Ahn-GiHwan <61266464+Ahn-GiHwan@users.noreply.github.com>
2021-03-22 14:30:50 +0900

    starbucks_4

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ searchInputEl.addEventListener('blur', function () {
 });
 
 const badgeEl = document.querySelector('header .badges');
+const toTolEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
   if (window.scrollY > 500) {
@@ -24,14 +25,27 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 0,
       display: 'none'
     });
+    gsap.to(toTolEl, 0.2, {
+      x: 0
+    });
   } else {
     gsap.to(badgeEl, 0.6, {
       opacity: 1,
       display: 'block'
     });
+    gsap.to(toTolEl, 0.2, {
+      x: 100
+    });
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+
+toTolEl.addEventListener('click', function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0
+  });
+})
 
 // MAIN IMAGE
 const fadeEls = document.querySelectorAll('.visual .fade-in');
@@ -70,6 +84,18 @@ new Swiper('.promotion .swiper-container', {
   }
 });
 
+// AWARDS
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+});
+
 // PROMOTION
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
@@ -100,3 +126,20 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', 0.5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+// ScrollMagic
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소 설정
+      triggerHook: 0.8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+// YEAR
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
+
